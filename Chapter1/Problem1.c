5e3f041e701f6de555a1f017408695b9fd0aec9d
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2024-08-29 17:20:00 -0800

    second deriv test

--- Chapter1/Problem1.c
+++ Chapter1/Problem1.c
@@ -3,7 +3,7 @@
 
 #define MIN 0.0
 #define MAX 2.0
-#define STEP 0.00000001
+#define STEP 0.000001
 #define EPSILON 1e-6
 
 
@@ -17,17 +17,23 @@ long double deriv(double x, double h)
   return (f(x+h) - (f(x-h))) / (2.0*h);
 }
 
+long double secderiv(double x, double h)
+{
+  return (f(x+h) - 2*(f(x)) + f(x-h)) / pow(h, 2);
+}
+
+
 int main()
 {
   long double h = 1e-5;
   double x;
   for (x = MIN; x <= MAX; x+= STEP){
     double derivout = deriv(x, h);
-    if (fabs(derivout) < EPSILON){
-      printf(" Critical Point found at x = %f, f(x) = %Lf\n", x, f(x));
-    }
+    double secderivout = secderiv(x, h);
+  //  printf(" Critical Point found at x = %f, f(x) = %Lf\n", x, f(x));
+    printf("second deriv x = %f, f(x)'' = %Lf\n", x, secderiv(x, h));
   }
-  printf("y = %Lf\n", f(5.0));
-  printf("deriv = %.30Lf\n", deriv(1.2598, 1e-5));
+ // printf("y = %Lf\n", f(5.0));
+ //printf("deriv = %.30Lf\n", deriv(1.2598, 1e-5));
   return 0;
 }

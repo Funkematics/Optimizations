8537478b6e6559cc11f798e6f127919fc0f43566
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2024-09-06 07:55:20 -0800

    removed old

--- Chapter1/Problem1.c
+++ Chapter1/Problem1.c
@@ -3,65 +3,99 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-//200,000 points as recommended
+//----------------------INTERFACE-------------------------
 #define MIN 0.0
 #define MAX 2.0
 #define STEP 0.000005
 #define EPSILON 1e-6
-
-
-struct feasibles {
-  double* xval;
-  uint32_t size;
-};
-
-struct extrema {
-  double* xval;
-  uint32_t size;
-};
+#define STORAGE 100
 
 double f(double x)
 {
-  return pow((pow(x, 2) + sin(x)), 2) - 10 * (cos( 5 * x) + (3.0/2.0) * x);
+    return pow((pow(x, 2) + sin(x)), 2) - 10 * (cos(5 * x) + (3.0/2.0) * x);
 }
+//----------------------------------------------------------
+//The above block is where we determine our range in MIN and MAX, our epsilon, steps,
+//max storage size for extrema, this will be replaced with a dynamic memory, and finally
+//our input function
 
+//derivative using central finite differences
 double deriv(double x, double h)
 {
-  return (f(x+h) - (f(x-h))) / (2.0*h);
+    return (f(x+h) - f(x-h)) / (2.0*h);
 }
-
+//second order deriv using central finite differences
 long double secderiv(double x, double h)
 {
-  return (f(x+h) - 2*(f(x)) + f(x-h)) / pow(h, 2);
+    return (f(x+h) - 2*f(x) + f(x-h)) / (h * h);
 }
 
-bool signCheck(double y1)
+bool signCheck(double prev, double curr)
 {
-  double y2 = y1 + STEP;
-
-  if ((y2 > 0 && y1 < 0) || (y2 < 0 && y1 > 0)){
-    return true;
-  }
-  return false;
+    // Check if the sign has changed, considering EPSILON for near-zero values
+    if ((prev > EPSILON && curr < -EPSILON) || (prev < -EPSILON && curr > EPSILON)) {
+        return true;
+    }
+    return false;
 }
+
+//Newtons Method for finding critical points
+double duke_newton(double x0, double h, double tolerance, int max_iterations)
+{
+  double x = x0;
+  double x_new;
+
+  for (int i = 0; i < max_iterations; i++){
+    x_new = x - deriv(x, h) / secderiv(x, h);
+
+    if (fabs(x_new - x) < tolerance) {
+      return x_new;
+    }
+    x = x_new;
+  }
+  printf("Did not converge within %d iterations for given range.\n", max_iterations);
+  return x_new;
+}
+
 
 int main()
 {
-  double h = 1e-5;
-  double x;
-  int signflips = 0;
-  for (x = MIN; x <= MAX; x+= STEP){
-    double derivout = deriv(x, h);
-    double secderivout = secderiv(x, h);
-    //printf("signchecks : %b \n", signCheck(secderivout));
-    if (signCheck(secderivout) == true) {
-      ++signflips;
+    double h = EPSILON; //Just for notational convenience
+    double x;
+    int inflectionscount = 1;  // This is more of a count of convexities or concavity.
+
+    int i = 0;         //index
+    double inflections[STORAGE];
+    inflections[i] = MIN;
+    ++i;
+
+    // Here we find inflections/concavities/convexities and put our data in an array.
+    double prev_deriv = secderiv(MIN, h);       // Start with the first value
+    for (x = MIN; x <= MAX; x += STEP) {        
+        double curr_deriv = secderiv(x, h);
+        if (signCheck(prev_deriv, curr_deriv)) { 
+            ++inflectionscount;
+            inflections[i] = x;
+            i++;
+            //printf("point : %f\n", x);
+        }
+        prev_deriv = curr_deriv;
+        //printf("second deriv x = %f, f(x)'' = %Lf\n", x, secderivrx, h));
     }
-  //  printf(" Critical Point found at x = %f, f(x) = %f\n", x, f(x));
-    printf("second deriv x = %f, f(x)'' = %Lf\n", x, secderiv(x, h));
-  }
- // printf("y = %Lf\n", f(5.0));
- //printf("deriv = %.30Lf\n", deriv(1.2598, 1e-5));
-   printf("sign flips : %d\n", signflips);
-  return 0;
+    inflections[i]  = MAX;
+
+    double guess;
+    double out;
+    for(i = 0; i <= inflectionscount; i++) {
+      guess = (inflections[i] + inflections[i-1]) / 2;
+     // printf("output : %f\n", out); 
+      if(i != 0) {
+        out = duke_newton(guess, h, EPSILON, 6);
+        printf("feasible x: %f        y: %f\n" , out, f(out));
+      }
+    }
+      
+
+    printf("curves : %d\n", inflectionscount);
+    return 0;
 }

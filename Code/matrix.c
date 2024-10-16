0f653ed713e9f83b18e28c691c4274ed130e69e3
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2024-10-16 00:43:04 -0800

    writing up problems I solved

--- Code/matrix.c
+++ Code/matrix.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_LENGTH 1000
 
-struct 2dmat {
-  double elem[][];
-  int sizei;
-  int sizej;
+typedef struct mat {
+  double **elem;
+  int i;
+  int j;
 
+} mat;
+
+mat* initMatrix(int rows, int cols)
+{
+  //For functions that operate on any matrix, we denote the matrix with capital letters internally
+  //We use A, B, C .... Z etc, this is standard in many math texts.
+  mat* A = (mat*)malloc(sizeof(mat));
+  if (A == NULL) {
+    fprintf(stderr, "Failure to allocate memory for matrix\n");
+    return NULL;
+  }
+
+  A->i = rows;
+  A->j = cols;
+
+  A->elem = (double**)malloc(rows * sizeof(double*));
+  if (A->elem == NULL) {
+    fprintf(stderr, "Failure to allocate memory for elements in matrix\n");
+    free(A);
+    return NULL;
+  }
+
+  for (int i = 0; i < rows; i++) {
+    A->elem[i] = (double*)malloc(cols * sizeof(int));
+    if (A->elem[i] == NULL){
+      fprintf(stderr, "Memory allocation failed\n");
+      for (int j = 0; j < i; j++) {
+        free(A->elem[j]);
+      }
+      free(A->elem);
+      free(A);
+      return NULL;
+    }
+  }
+return A;
 }
 
 int main(void)
 {
-  FILE *matrix;
-  char line[MAX_LENGTH];
-
-  file = fopen("test.mat", "r");
-  if (file == NULL) {
-    perror("Error opening file");
-    return -1'
+  return 0;
 }
-
